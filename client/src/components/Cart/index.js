fa92118e486d3540f82620e1ad45a18a24eea8a7
HacAtac <jhack00@icloud.com>
2022-03-03 04:21:21 -0600

    shopping cart UI and logic complete

---
+++ client/src/components/Cart/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import CartItem from "../CartItem";
+import Auth from "../../utils/auth";
+import "./style.css";
+import { useStoreContext } from "../../utils/GlobalState";
+import { TOGGLE_CART } from "../../utils/actions";
+
+const Cart = () => {
+  const [state, dispatch] = useStoreContext();
+  //   console.log(state);
+
+  function toggleCart() {
+    dispatch({ type: TOGGLE_CART });
+  }
+
+  function calculateTotal() {
+    let sum = 0;
+    state.cart.forEach((item) => {
+      //what is forEach doing here? // its looping through the cart and adding the price to the sum
+      sum += item.price * item.purchaseQuantity; //what is += ? // it means sum = sum + item.price
+      //the * is saying that the price is multiplied by the purchase quantity
+    });
+    return sum.toFixed(2); //what is toFixed? // its a javascript method that rounds a number to a certain number of decimal places
+  }
+
+  if (!state.cartOpen) {
+    return (
+      <div className="cart-closed" onClick={toggleCart}>
+        <span role="img" aria-label="trash">
+          🛒
+        </span>
+      </div>
+    );
+  }
+  return (
+    <div className="cart">
+      <div className="close" onClick={toggleCart}>
+        [close]
+      </div>
+      <h2>Shopping Cart</h2>
+      {state.cart.length ? (
+        <div>
+          {state.cart.map((item) => (
+            <CartItem Key={item._id} item={item} />
+          ))}
+          <div className="flex-row space-between">
+            <strong>Total: ${calculateTotal()}</strong>
+            {Auth.loggedIn() ? (
+              <button>Checkout</button>
+            ) : (
+              <span>(log in to checkout)</span>
+            )}
+          </div>
+        </div>
+      ) : (
+        <h3>
+          <span role="img" aria-label="shocked">
+            😱
+          </span>
+          You haven't added anything to your cart yet!
+        </h3>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
